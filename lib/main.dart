767f0b9601ab29c03c3eec4f6481dea677928a75
Vinicius Purtan <vpurtan@gmail.com>
2025-07-19 16:19:31 +0200

    feat: initialize date formatting for Spanish locale
    Add intl package and initialize date formatting for Spanish locale before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shiftsense/theme.dart';
 import 'package:shiftsense/screens/home_page.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es', null);
   runApp(const MyApp());
 }
 
