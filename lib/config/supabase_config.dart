a7134aeee00ca752fbc8a73e897f43fece08b169
Vinicius Purtan <vpurtan@gmail.com>
2025-07-19 18:08:44 +0200

    Add Supabase configuration file

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,8 @@
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://jqyvdhqirhdfpztaudep.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxeXZkaHFpcmhkZnB6dGF1ZGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5Mjk2MjgsImV4cCI6MjA2ODUwNTYyOH0.RVGU4_Nwew6JD9nX4hz_QfeWgCHTz3ur17F1Yu-BLO0';
+  
+  // Service role key - Solo para operaciones administrativas
+  // ⚠️ NUNCA exponer en el cliente en producción
+  static const String supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxeXZkaHFpcmhkZnB6dGF1ZGVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjkyOTYyOCwiZXhwIjoyMDY4NTA1NjI4fQ.RVGU4_Nwew6JD9nX4hz_QfeWgCHTz3ur17F1Yu-BLO0';
+}
